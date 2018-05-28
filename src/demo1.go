1ee22ba38510075d1ad24ec0d6d313fa376a26ca
wangxuguang <nova_wangxuguang@126.com>
2018-05-28 11:10:09 +0800

    学习Go语言

---
+++ src/demo1.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//testArray()
+	testPoint()
+}
+
+func testPoint() {
+	var a int = 20 /* 声明实际变量 */
+	var ip *int    /* 声明指针变量 */
+
+	ip = &a /* 指针变量的存储地址 */
+
+	fmt.Printf("a 变量的地址是: %x\n", &a)
+
+	/* 指针变量的存储地址 */
+	fmt.Printf("ip 变量储存的指针地址: %x\n", ip)
+
+	/* 使用指针访问值 */
+	fmt.Printf("*ip 变量的值: %d\n", *ip)
+}
+
+func testArray() {
+	var n [10]int
+	/* n 是一个长度为 10 的数组 */
+	var i, j int
+	/* 为数组 n 初始化元素 */
+	for i = 0; i < 10; i++ {
+		n[i] = i + 100 /* 设置元素为 i + 100 */
+	}
+	/* 输出每个数组元素的值 */
+	for j = 0; j < 10; j++ {
+		fmt.Printf("Element[%d] = %d\n", j, n[j])
+	}
+}
